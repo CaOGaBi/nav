7291533538735055349775d273d855e9cdb052f0
CaoGaBi <848276721@qq.com>
2022-03-02 18:56:27 +0800

    change repo url

--- nav.config.ts
+++ nav.config.ts
@@ -1,9 +1,16 @@
+/*
+ * @Author: Whilding
+ * @Date: 2022-03-02 18:25:59
+ * @LastEditors: Whilding
+ * @LastEditTime: 2022-03-02 18:55:04
+ * @Description: file content
+ */
 import { IConfig } from './src/types'
 
 const c: IConfig = {
   // [Mondatory], Please replace following Github url with your own Github address which you forked in.
   // [必填], 请填写您的仓库地址
-  gitRepoUrl: 'https://github.com/xjh22222228/nav',
+  gitRepoUrl: 'https://github.com/CaOGaBi/nav',
 
   // Deployment branch name
   // 部署分支
